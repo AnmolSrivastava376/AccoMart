460c1e6b4799388c02f240f9dd21745e2cf7ab2a
KalikaAccolite <162412940+KalikaAccolite@users.noreply.github.com>
2024-04-25 16:08:09 +0530

    Landing page html

--- Frontend/AccoMart/src/app/app.routes.ts
+++ Frontend/AccoMart/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
@@ -7,14 +7,20 @@ import { OrdersComponent } from './pages/orders/orders.component';
 import { PaymentComponent } from './pages/payment/payment.component';
 import { YourOrdersComponent } from './pages/your-orders/your-orders.component';
 import { InvoiceComponent } from './pages/invoice/invoice.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {path:'', component:LandingPageComponent},
-    {path:'/home', component:HomeComponent},
-    {path:'/home/cart', component:CartComponent},
-    {path:'/home/auth', component:AuthComponent},
-    {path:'/home/cart/orders', component:OrdersComponent},
-    {path:'/home/cart/payment', component:PaymentComponent},
-    {path:'/home/yourorders',component:YourOrdersComponent},
-    {path:'/home/cart/orders/invoice', component:InvoiceComponent}
+    {path:'home', component:HomeComponent},
+    {path:'home/cart', component:CartComponent},
+    {path:'home/auth', component:AuthComponent},
+    {path:'home/cart/orders', component:OrdersComponent},
+    {path:'home/cart/payment', component:PaymentComponent},
+    {path:'home/yourorders',component:YourOrdersComponent},
+    {path:'home/cart/orders/invoice', component:InvoiceComponent}
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+  export class AppRoutingModule { }
