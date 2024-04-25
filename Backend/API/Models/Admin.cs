38de247766c195eab4ddabd50f9204859d96f00d
khushboo2111 <162410725+khushboo2111@users.noreply.github.com>
2024-04-25 10:28:50 +0530

    Create/ApplicationDbContext

--- Backend/API/Models/Admin.cs
+++ Backend/API/Models/Admin.cs
@@ -2,5 +2,8 @@
 {
     public class Admin
     {
+        public int AdminId { get; set; }
+        public string AdminEmail { get; set; }
+        public string AdminPassword { get; set; }
     }
 }
