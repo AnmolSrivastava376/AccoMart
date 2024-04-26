39db5dea2b0a495db75f13dfe4522cf75d67e979
KalikaAccolite <162412940+KalikaAccolite@users.noreply.github.com>
2024-04-26 12:56:04 +0530

    category and product service

---
+++ Frontend/AccoMart/src/app/services/category.services.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { Category } from '../interfaces/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  fetchCategories() {
+    return axios.get<Category[]>('http://localhost:5239/AdminDashboard');
+  }
+}
