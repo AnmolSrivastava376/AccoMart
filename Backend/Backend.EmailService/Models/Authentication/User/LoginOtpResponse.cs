6c3908b809ff87998f04e515aef8451cf1188a11
khushboo2111 <162410725+khushboo2111@users.noreply.github.com>
2024-04-28 08:28:51 +0530

    Migrated DbContext files to Data project

---
+++ Backend/Backend.EmailService/Models/Authentication/User/LoginOtpResponse.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Models.Authentication.User
+{
+    public class LoginOtpResponse
+    {
+        public string Token { get; set; } = null;
+        public bool IsTwoFactorEnable { get; set; }
+        public IdentityUser User { get; set; } = null;
+    }
+}
