3a3da891f639cec1520674b0284767577f0314c4
KalikaAccolite <162412940+KalikaAccolite@users.noreply.github.com>
2024-04-29 11:47:29 +0530

    Changes

---
+++ Frontend/AccoMart/src/app/components/product-detail-card/product-detail-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-detail-card.component.html',
+  styleUrl: './product-detail-card.component.css'
+})
+export class ProductDetailCardComponent {
+
+}
