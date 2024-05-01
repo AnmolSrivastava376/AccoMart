f95f0cb9baddb99ba4ef198ebccb8892c47b16d6
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-05-01 13:08:09 +0530

    orderController

---
+++ Backend/API/Models/DTO/ProductOrderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models.DTO
+{
+    public class ProductOrderDto
+    {
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public int ProductId { get; set; } 
+        public int DeliveryServiceID { get; set; }
+
+
+    }
+}
--- Backend/API/Models/Orders.cs
+++ Backend/API/Models/Orders.cs
@@ -4,11 +4,19 @@
     {
         public int OrderId { get; set; } 
         public DateTime OrderDate { get; set; }
-
-        public TimeSpan OrderTime { get; set; }
-
-        public int AddressId { get; set; }
-
         public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public float OrderAmount {  get; set; }
+        public TimeOnly OrderTime { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int CartId { get; set; }
+
+        public int DeliveryServiceID { get; set; }
+
+
+
+
     }
 }
