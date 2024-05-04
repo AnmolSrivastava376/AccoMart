245b6a4fa3223bdc181c55cc194dc3176c389846
khushboo2111 <162410725+khushboo2111@users.noreply.github.com>
2024-05-04 12:36:50 +0530

    Update ApplicationDbContext.cs

--- Backend/Data/Models/ApplicationDbContext.cs
+++ Backend/Data/Models/ApplicationDbContext.cs
@@ -1,14 +1,30 @@
-﻿
-
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace Data.Models
 {
-    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
-        public DbSet<Admin> admins {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData
+                (
+                new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                new IdentityRole() { Name = "User", ConcurrencyStamp = "1", NormalizedName = "User" }
+                );
+        }
+
     }
 }
