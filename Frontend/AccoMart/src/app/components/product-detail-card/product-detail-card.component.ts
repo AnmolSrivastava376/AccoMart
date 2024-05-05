bd0fb75b5d752593f6459ea9c7b2d3253a98d2c9
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-05-05 13:42:51 +0530

    frontend changes

--- Frontend/AccoMart/src/app/components/product-detail-card/product-detail-card.component.ts
+++ Frontend/AccoMart/src/app/components/product-detail-card/product-detail-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../interfaces/product';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-product-detail-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './product-detail-card.component.html',
   styleUrl: './product-detail-card.component.css'
 })
