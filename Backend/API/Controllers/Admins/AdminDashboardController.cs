ea63a341b127b87cfa22d785e001113f7d509b9b
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-05-06 12:11:48 +0530

    removed authorize from admin controllers

--- Backend/API/Controllers/Admins/AdminDashboardController.cs
+++ Backend/API/Controllers/Admins/AdminDashboardController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers.Admins
 {
-    [Authorize(Roles="Admin")]
     [Route("AdminDashboard")]
     [ApiController]
     public class AdminDashboardController : Controller
