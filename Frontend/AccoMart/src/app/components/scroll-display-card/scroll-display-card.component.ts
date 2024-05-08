1d6300b30ea4a25ed67189d5bc794cb918ed8384
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-05-08 23:27:18 +0530

    Fetch Images

--- Frontend/AccoMart/src/app/components/scroll-display-card/scroll-display-card.component.ts
+++ Frontend/AccoMart/src/app/components/scroll-display-card/scroll-display-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../interfaces/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scroll-display-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './scroll-display-card.component.html',
   styleUrl: './scroll-display-card.component.css'
 })
 export class ScrollDisplayCardComponent {
-
+  @Input() products?:Product[];
 }
