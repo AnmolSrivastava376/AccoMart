b1c8c3e64e24afe358175c89df5097a93ebe12c2
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-05-08 12:44:48 +0530

    Homepage

--- Frontend/AccoMart/src/app/components/product-card/product-card.component.ts
+++ Frontend/AccoMart/src/app/components/product-card/product-card.component.ts
@@ -3,11 +3,13 @@ import { Product } from '../../interfaces/product';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { GridDisplayCardComponent } from '../grid-display-card/grid-display-card.component';
+import { ScrollDisplayCardComponent } from '../scroll-display-card/scroll-display-card.component';
+import { ProductScrollDisplayCardComponent } from '../product-scroll-display-card/product-scroll-display-card.component';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule,GridDisplayCardComponent],
+  imports: [CommonModule,GridDisplayCardComponent,ScrollDisplayCardComponent, ProductScrollDisplayCardComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
---
+++ Frontend/AccoMart/src/app/components/product-scroll-display-card/product-scroll-display-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductScrollDisplayCardComponent } from './product-scroll-display-card.component';
+
+describe('ProductScrollDisplayCardComponent', () => {
+  let component: ProductScrollDisplayCardComponent;
+  let fixture: ComponentFixture<ProductScrollDisplayCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProductScrollDisplayCardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ProductScrollDisplayCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
