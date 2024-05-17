5f271a6ab3db398caba95c0ba23f671c18bccf42
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-05-17 12:19:35 +0530

    added popup for adding and editing delivery services

---
+++ Frontend/AccoMart/src/app/components/edit-category/edit-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-category.component.html',
+  styleUrl: './edit-category.component.css'
+})
+export class EditCategoryComponent {
+
+}
--- Frontend/AccoMart/src/app/pages/admin-categories/admin-categories.component.ts
+++ Frontend/AccoMart/src/app/pages/admin-categories/admin-categories.component.ts
@@ -4,12 +4,14 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { CategoryService } from '../../services/category.services';
 import { Category } from '../../interfaces/category';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-categories',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent,CommonModule],
+  imports: [NavbarComponent, SidebarComponent,CommonModule,FormsModule],
   templateUrl: './admin-categories.component.html',
   styleUrl: './admin-categories.component.css'
 })
@@ -17,9 +19,20 @@ export class AdminCategoriesComponent implements OnInit {
   constructor(private router: Router,private categoryService:CategoryService) { }
   categories: Category[] = [];
 
+    selectedCategory:Category = {
+      categoryId:-1,
+      categoryName:''
+   };  
+
+   defaultCategory:Category = {
+    categoryId:-1,
+    categoryName:''
+ };  
+
   ngOnInit(): void {
     this.fetchCategories();
   }
+  isEditPopupOpen:boolean = false;
 
   fetchCategories() {
     this.categoryService.fetchCategories().then(response => {
@@ -41,8 +54,25 @@ export class AdminCategoriesComponent implements OnInit {
     // Implement delete functionality
   }
 
-  editCategory(categoryId: number) {
-    // Implement edit functionality
+  editCategory(category: Category) {
+    this.selectedCategory = { ...category }; 
+    this.isEditPopupOpen = true; 
+    
+  }
+
+  openEditPopup(category: Category){
+    this.isEditPopupOpen = true;
+    this.selectedCategory = { ...category }; 
+    console.log(this.selectedCategory);
+  }
+  closeEditPopup()
+  {
+    this.isEditPopupOpen = false;
+  }
+
+  saveChanges()
+  {
+
   }
 
 }
