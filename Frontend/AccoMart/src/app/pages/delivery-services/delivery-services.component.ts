f104e97bc2893ea93137080029c7ae19048457b6
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-05-19 12:07:30 +0530

    admin delivery working with interceptor

--- Frontend/AccoMart/src/app/pages/delivery-services/delivery-services.component.ts
+++ Frontend/AccoMart/src/app/pages/delivery-services/delivery-services.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { deliveryServices } from '../../services/delivery.service';
+import { deliveryService } from '../../services/delivery.service';
 import { DeliveryService } from '../../interfaces/deliveryService';
 import { CommonModule } from '@angular/common';
 import { createDeliveryService } from '../../interfaces/createDeliveryService';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-delivery-services',
   standalone: true,
-  imports: [SidebarComponent,NavbarComponent,CommonModule,FormsModule],
+  providers:[deliveryService],
+  imports: [SidebarComponent,NavbarComponent,CommonModule,FormsModule,HttpClientModule],
   templateUrl: './delivery-services.component.html',
   styleUrl: './delivery-services.component.css'
 })
 export class DeliveryServicesComponent {
 
-  constructor(private deliveryService: deliveryServices) {}
+  constructor(private deliveryService: deliveryService) {}
   deliveryServicesList: DeliveryService[] = [];
   openAddServicePopup:boolean = false;
   openEditServicePopup:boolean = false;
@@ -39,73 +41,72 @@ export class DeliveryServicesComponent {
   ngOnInit(): void {
     this.fetchDeliveryServices();
   }
-  fetchDeliveryServices() {
-    this.deliveryService.getDeliveryServices().then(response => {
-      this.deliveryServicesList = response.data;
-    }).catch(error => {
-      console.error('Error fetching delivery services:', error);
-    });
 
-    
+  fetchDeliveryServices() {
+    this.deliveryService.getDeliveryServices().subscribe(
+      (response: DeliveryService[]) => {
+        this.deliveryServicesList = response;
+      },
+      error => {
+        console.error('Error fetching delivery services:', error);
+      }
+    );
   }
 
   deleteDeliveryService(id: number) {
-    
-    if(confirm("Are you sure you want to delete this Service?"))
-      {
-        this.deliveryService.deleteDeliveryService(id).then(() => {
+    if(confirm("Are you sure you want to delete this Service?")) {
+      this.deliveryService.deleteDeliveryService(id).subscribe(
+        () => {
           this.fetchDeliveryServices();
-        }).catch(error => {
+        },
+        error => {
           console.error('Error deleting delivery service:', error);
-        });
-      }
+        }
+      );
+    }
   }
 
-
   editDeliveryService(deliveryService: createDeliveryService) {
-    this.deliveryService.editDeliveryService(deliveryService,this.editServiceId).then(() => {
-      console.log("Delivery Service succesfully updated");
-      this.openAddServicePopup= false;
-      this.fetchDeliveryServices();
-    }).catch(error => {
-      console.error('Error editing delivery service:', error);
-    });
+    this.deliveryService.editDeliveryService(deliveryService,this.editServiceId).subscribe(
+      () => {
+        console.log("Delivery Service successfully updated");
+        this.openAddServicePopup= false;
+        this.fetchDeliveryServices();
+      },
+      error => {
+        console.error('Error editing delivery service:', error);
+      }
+    );
   }
 
   createDeliveryService(newDeliveryService: createDeliveryService) {
-    this.deliveryService.addDeliveryService(newDeliveryService).then(() => {
-      // After successful creation, fetch the updated list of delivery services
-      this.openAddServicePopup =false;
-      this.fetchDeliveryServices();
-    }).catch(error => {
-      console.error('Error creating delivery service:', error);
-    });
+    this.deliveryService.addDeliveryService(newDeliveryService).subscribe(
+      () => {
+        // After successful creation, fetch the updated list of delivery services
+        this.openAddServicePopup =false;
+        this.fetchDeliveryServices();
+      },
+      error => {
+        console.error('Error creating delivery service:', error);
+      }
+    );
   }
 
-  openAddPopup()
-  {
+  openAddPopup() {
     this.openAddServicePopup = true;
   }
 
-  openEditPopup(Id:number,service:createDeliveryService)
-  {
+  openEditPopup(Id:number,service:createDeliveryService) {
     this.editServiceId = Id;
     this.openAddServicePopup = true;
     this.serviceToEdit = {...service};
-
   }
 
-  
-  closeAddPopup()
-  {
+  closeAddPopup() {
     this.openAddServicePopup = false;
   }
 
-  closeEditPopup()
-  {
+  closeEditPopup() {
     this.openAddServicePopup = false;
   }
-
 }
-
-
