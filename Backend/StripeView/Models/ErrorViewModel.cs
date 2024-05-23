a227c31d3695b6304aced6f118ca840bc5b4be0c
khushboo2111 <162410725+khushboo2111@users.noreply.github.com>
2024-05-23 07:48:17 +0530

    Delete Backend/StripeView directory

--- Backend/StripeView/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StripeView.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
