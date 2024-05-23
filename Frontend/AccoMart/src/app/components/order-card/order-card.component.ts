be9fa2c70608196c33d2ae669ff25e8db7ffa818
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-05-23 16:33:28 +0530

    Fetch orders

--- Frontend/AccoMart/src/app/components/order-card/order-card.component.ts
+++ Frontend/AccoMart/src/app/components/order-card/order-card.component.ts
@@ -1,29 +1,35 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Order } from '../../interfaces/order';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { LoaderComponent } from '../loader/loader.component';
+import { addressService } from '../../services/address.service';
 
 @Component({
-    selector: 'app-order-card',
-    standalone: true,
-    templateUrl: './order-card.component.html',
-    styleUrl: './order-card.component.css',
-    imports: [CommonModule, HttpClientModule, LoaderComponent]
+  selector: 'app-order-card',
+  standalone: true,
+  templateUrl: './order-card.component.html',
+  styleUrl: './order-card.component.css',
+  imports: [CommonModule, HttpClientModule, LoaderComponent],
+  providers: [addressService]
 })
-export class OrderCardComponent{
- @Input() orders?:Order[]
- format(date: Date): string {
-  const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
+export class OrderCardComponent implements OnInit{
+  @Input() orders?: Order[];
 
-  const month = months[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
+  constructor(private addressService: addressService){}
 
-  return `${month} ${day}, ${year}`;
-}
+  ngOnInit(): void {
+  }
 
+  format(dateStr: Date): string {
+    const date = new Date(dateStr)
+    const months = [
+      "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"
+    ];
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${month} ${day}, ${year}`;
+  }
 }
--- Frontend/AccoMart/src/app/pages/your-orders/your-orders.component.ts
+++ Frontend/AccoMart/src/app/pages/your-orders/your-orders.component.ts
@@ -7,6 +7,7 @@ import { orderService } from '../../services/order.service';
 import { jwtDecode } from 'jwt-decode';
 import { HttpClientModule } from '@angular/common/http';
 import { deliveryService } from '../../services/delivery.service';
+import { addressService } from '../../services/address.service';
 
 @Component({
   selector: 'app-your-orders',
@@ -14,15 +15,15 @@ import { deliveryService } from '../../services/delivery.service';
   imports: [CommonModule, NavbarComponent, OrderCardComponent, HttpClientModule],
   templateUrl: './your-orders.component.html',
   styleUrl: './your-orders.component.css',
-  providers: [orderService, deliveryService]
+  providers: [orderService, deliveryService, addressService]
 })
 export class YourOrdersComponent implements OnInit {
   orders:Order[] = []
-  currentOrders: Order[];
-  historyOrders: Order[];
+  currentOrders: Order[]=[];
+  historyOrders: Order[]=[];
   userId: string
   decoded: any
-  constructor(private orderService: orderService, private deliveryService: deliveryService) {
+  constructor(private orderService: orderService, private deliveryService: deliveryService, private addressService: addressService) {
   }
   ngOnInit(): void {
     const token = localStorage.getItem('token')
@@ -32,19 +33,42 @@ export class YourOrdersComponent implements OnInit {
       this.orderService.fetchAllOrders(this.userId).subscribe(
         response=>{
           this.orders = response;
+          this.addAddresses();
           this.sortOrders();
         }
       )
     }
   }
+  addAddresses(){
+    this.orders.forEach(order=>{
+      this.addressService.getAddressByAddressId(order.addressId).subscribe(
+        response=>{
+          order.address = response.street + ", " + response.city + response.state + " - " + response.zipCode
+        }
+      )
+    })
+  }
   sortOrders(){
     const currentDate = new Date();
     this.orders.forEach(order=>{
-      const deliveryDays = this.deliveryService.getDeliveryDate(order.deliveryServiceId)
-      const orderDate = new Date(order.orderDate);
-      // if(orderDate + 3){
-
-      // }
+      let deliveryDays=0;
+      this.deliveryService.getDeliveryDate(order.deliveryServiceID).subscribe(
+        response=>{
+          deliveryDays = response;
+          const orderDate = new Date(order.orderDate);
+          order.expectedDate = new Date(orderDate.getTime() + (deliveryDays * 24 * 60 * 60 * 1000));
+          if (currentDate > order.expectedDate) {
+            order.isDelivered = true;
+          } else {
+            order.isDelivered = false;
+          }
+          if (!order.isCancelled && !order.isDelivered) {
+            this.currentOrders.push(order);
+          } else {
+            this.historyOrders.push(order);
+          }
+        }
+      )
     });
   }
 }
