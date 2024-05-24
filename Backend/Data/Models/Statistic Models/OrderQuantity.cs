ad8587783a61b92865b206d7e942d01e3182980c
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-05-24 11:35:03 +0530

    Added Chart Controllers

---
+++ Backend/Data/Models/Statistic Models/OrderQuantity.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Models.Statistic_Models
+{
+    public class OrderQuantity
+    {
+        public int Count { get; set; }
+        public DateTime OrderDate { get; set; }
+        public long TotalSales { get; set; } = 0;
+    }
+}
