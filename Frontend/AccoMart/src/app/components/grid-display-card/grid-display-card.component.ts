86116b2d46bb331f16e8635fafba28cf222a50b8
KalikaAccolite <162412940+KalikaAccolite@users.noreply.github.com>
2024-05-28 09:20:05 +0530

    test grid

--- Frontend/AccoMart/src/app/components/grid-display-card/grid-display-card.component.ts
+++ Frontend/AccoMart/src/app/components/grid-display-card/grid-display-card.component.ts
@@ -1,20 +1,36 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Product } from '../../interfaces/product';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-grid-display-card',
   standalone: true,
-  imports: [CommonModule,HttpClientModule],
+  imports: [CommonModule,HttpClientModule,MatIcon],
   templateUrl: './grid-display-card.component.html',
   styleUrl: './grid-display-card.component.css'
 })
 export class GridDisplayCardComponent{
   @Input() products?: Product[]
-
+  @Output() fetchNextPage: EventEmitter<boolean> = new EventEmitter<boolean>();
+  i=0;
   navigateToProductDetail(index:number){
     const productId = this.products? this.products[index].productId : 0;
     window.location.href=`home/productdetail/${productId}`
+  }
+
+  handleNextButtonClick() {
+    if (this.products && this.i + 2 < this.products.length) {
+      this.i++;
+      if(this.i+5>=this.products.length){
+        this.fetchNextPage.emit(true);
+      }
+    }
+  }
+  handlePreviousButtonClick() {
+    if (this.i > 0) {
+      this.i--;
+    }
   }
 }
