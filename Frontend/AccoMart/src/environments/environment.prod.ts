19baab2df7ddd0d34b63fa9076b872da8fe60030
khushboo2111 <khushboo.gupta1@accolitedigital.com>
2024-06-04 10:53:16 +0530

    modifications

--- Frontend/AccoMart/src/environments/environment.prod.ts
+++ Frontend/AccoMart/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : true,
     url : 'https://agreeable-coast-00adf000f.5.azurestaticapps.net',
-    serverUrl : 'accomartserver.azurewebsites.net/'
+    serverUrl : 'https://accomartserver.azurewebsites.net/'
 };
