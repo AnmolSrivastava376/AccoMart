e8d1c35b3a0c23645dc3fe190fa74fe6dd402905
khushboo2111 <khushboo.gupta1@accolitedigital.com>
2024-06-04 11:03:36 +0530

    modifications

--- Frontend/AccoMart/src/app/services/address.service.ts
+++ Frontend/AccoMart/src/app/services/address.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
-export class AddressService {
+export class addressService {
   constructor(private http: HttpClient) {}
 
   baseUrl = environment.serverUrl;
