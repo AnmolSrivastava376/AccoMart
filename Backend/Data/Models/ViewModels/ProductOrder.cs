3d56e8fb94b9c501b7469eeb813860c39025e5b5
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-06-19 13:19:20 +0530

    Final fixes (#347)
    * Email body modified
    
    * router link problem fixed navbar
    
    * router link problem fixed navbar
    
    * router link problem fixed navbar
    
    * confirming while cancelling order
    
    * modification
    
    * router link changed
    
    * category search bar fixed
    
    * sidebar fixes
    
    * load more fixed
    
    * fixes in adding product
    
    * undo sidebar changes
    
    * undo sidebar changes
    
    * form validation fixed
    
    * form validation fixed
    
    * form validation fixed
    
    * icon
    
    * icon changes
    
    * otp filed changes
    
    * otp filed changes
    
    * Update README.md
    
    * address controller modified
    
    * create category
    
    * model state validation
    
    * model state validation

--- Backend/Data/Models/ViewModels/ProductOrder.cs
+++ Backend/Data/Models/ViewModels/ProductOrder.cs
@@ -1,11 +1,18 @@
-﻿namespace Data.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models.ViewModels
 {
     public class ProductOrder
     {
+        [Required(ErrorMessage ="Error message is requied")]
         public string UserId { get; set; }
+        [Required(ErrorMessage ="Address Id is requied")]
         public int AddressId { get; set; }
+        [Required(ErrorMessage ="Delivery Id is requied")]
         public int DeliveryId{ get; set; }
+        [Required(ErrorMessage = "Product Id is requied")]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Quantity is required")]
         public int quantity   {  get; set; }
 
     }
