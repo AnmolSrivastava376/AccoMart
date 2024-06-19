99b1a5b7c4d199772c63598adb46513d5d633a63
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-06-19 12:58:47 +0530

    Final fixes (#346)
    * Email body modified
    
    * router link problem fixed navbar
    
    * router link problem fixed navbar
    
    * router link problem fixed navbar
    
    * confirming while cancelling order
    
    * modification
    
    * router link changed
    
    * category search bar fixed
    
    * sidebar fixes
    
    * load more fixed
    
    * fixes in adding product
    
    * undo sidebar changes
    
    * undo sidebar changes
    
    * form validation fixed
    
    * form validation fixed
    
    * form validation fixed
    
    * icon
    
    * icon changes
    
    * otp filed changes
    
    * otp filed changes
    
    * Update README.md
    
    * address controller modified
    
    * create category
    
    * model state validation

--- Backend/Data/Models/Authentication/User/Admin.cs
+++ Backend/Data/Models/Authentication/User/Admin.cs
@@ -1,9 +1,14 @@
-﻿namespace Data.Models.Authentication.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models.Authentication.User
 {
     public class Admin
     {
+        [Required]
         public int AdminId { get; set; }
+        [EmailAddress]
         public string AdminEmail { get; set; }
+        [Required]
         public string AdminPassword { get; set; }
     }
 }
