da4a86e6f3cb2a3d21a2f9df01e5cebf66cf32f9
khushboo2111 <khushboo.gupta1@accolitedigital.com>
2024-06-20 13:16:26 +0530

    admin changes

--- Backend/Data/Repository/Interface/IProductRepository.cs
+++ Backend/Data/Repository/Interface/IProductRepository.cs
@@ -8,15 +8,15 @@ namespace Data.Repository.Interfaces
     public interface IProductRepository
     {
 
-        Task<Product> GetProductById(int id);
-        Task<Category> GetCategoryByName(string name);
+        Task<Product> GetProductById(int id, string userId);
+        Task<Category> GetCategoryByName(string name, string userId);
         Task<List<Product>> GetAllProductsByCategory(int id,string orderBy);
         Task<List<Product>> GetAllProductsPagewise(int pageNo, int pageSize, string userId);
         Task<List<Product>> GetProductsByPageNo(int id, int pageNo, int pageSize);
         Task<List<Product>> GetAllProducts();
         Task<List<Category>> GetAllCategories();
         Task<List<Category>> GetAllCategoriesAdmin(string userId);
-        Task<Category> GetCategoryById(int id);
+        Task<Category> GetCategoryById(int id,string userId);
         Task<Category> CreateCategory(string categoryName);
         Task<Product> CreateProduct(ViewProduct productDto,string userId);
         Task<Category> UpdateCategory(int Id, string NewCategoryName);
@@ -24,6 +24,7 @@ namespace Data.Repository.Interfaces
         Task DeleteCategory(int CategoryId);
         Task DeleteProduct(int productId);
         Task<List<Product>> GetProductBySearchName(string prefix);
+        Task<List<Product>> GetProductBySearchNameAdmin(string userId, string prefix);
         Task<List<Product>> GetProductsByCategoryName(string name);      
 
     }
